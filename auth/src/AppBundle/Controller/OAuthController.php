b7e3e18c148985359b35310d6ab3e92ff7f38471
sergej.moskeet <sergej.moskeet@gmail.com>
2018-12-28 11:23:37 +0200

    auth: OAuth Client added, but not configured

---
+++ auth/src/AppBundle/Controller/OAuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OAuthController extends Controller
+{
+    /**
+     * Link to this controller to start the "connect" process
+     *
+     * @Route("/connect/video", name="app_connect_video_start")
+     */
+    public function connectAction(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('video_oauth')
+            ->redirect(['video'])
+        ;
+    }
+
+    /**
+     * After going to Facebook, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     *
+     * @Route("/connect/video/check", name="app_connect_video_check")
+     */
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        // ** if you want to *authenticate* the user, then
+        // leave this method blank and create a Guard authenticator
+        // (read below)
+
+        /** @var \KnpU\OAuth2ClientBundle\Client\Provider\FacebookClient $client */
+        $client = $clientRegistry->getClient('video_oauth');
+
+        try {
+            // the exact class depends on which provider you're using
+            /** @var \League\OAuth2\Client\Provider\FacebookUser $user */
+            $user = $client->fetchUser();
+
+            // do something with all this new power!
+            // e.g. $name = $user->getFirstName();
+            var_dump($user); die;
+            // ...
+        } catch (IdentityProviderException $e) {
+            // something went wrong!
+            // probably you should return the reason to the user
+            var_dump($e->getMessage()); die;
+        }
+    }
+}
